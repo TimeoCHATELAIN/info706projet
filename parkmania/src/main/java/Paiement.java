b3a39185255724897a32fb7f42b86de02a37c310
nathan-roi <nathan.roi@disroot.org>
2025-10-13 10:15:25 +0200

    project setup again

---
+++ parkmania/src/main/java/Paiement.java
@@ -0,0 +1,20 @@
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+public class Paiement implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime datePaiement;
+    private double montant;
+    private String typePaiement; // "CB" ou "especes"
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
+
+    // Getters et setters
+}
---
+++ parkmania/src/main/java/Ticket.java
@@ -0,0 +1,19 @@
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Ticket implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime dateEntree;
+    private LocalDateTime dateSortie;
+
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+    private List<Paiement> paiements;
+
+    // Getters et setters
+}
