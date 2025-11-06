868e584cf2c93e549bcbc1ce8d4bc81317820ba9
nathan-roi <nathan.roi@disroot.org>
2025-11-06 14:15:34 +0100

    ajout des getters et setters dans les entites

--- parkmania/src/main/java/parkmania/entite/Paiement.java
+++ parkmania/src/main/java/parkmania/entite/Paiement.java
@@ -5,19 +5,47 @@ import jakarta.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+
+
 @Entity
 public class Paiement implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime datePaiement;
     private double montant;
-    private String typePaiement; // "CB" ou "especes"
 
-    @ManyToOne
+    public enum TypePaiment {CB, ESPECE};
+    private TypePaiment paiement;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ticket_id")
     private Ticket ticket;
 
+    public Paiement() {
+    }
+
+    public Paiement(double montant, TypePaiment typePaiement, Ticket ticket) {
+        this.datePaiement = LocalDateTime.now();
+        this.montant = montant;
+        this.paiement = typePaiement;
+        this.ticket = ticket;
+    }
+
     // Getters et setters
+    public Long getId() { return id; }
+
+    public LocalDateTime getDatePaiement() { return datePaiement; }
+    public void setDatePaiement(LocalDateTime datePaiement) { this.datePaiement = datePaiement; }
+
+    public double getMontant() { return montant; }
+    public void setMontant(double montant) { this.montant = montant; }
+
+    public TypePaiment getTypePaiement() { return paiement; }
+    public void setTypePaiement(TypePaiment typePaiement) { this.paiement = typePaiement; }
+
+    public Ticket getTicket() { return ticket; }
+    public void setTicket(Ticket ticket) { this.ticket = ticket; }
 }
--- parkmania/src/main/java/parkmania/entite/Ticket.java
+++ parkmania/src/main/java/parkmania/entite/Ticket.java
@@ -16,8 +16,28 @@ public class Ticket implements Serializable {
     private LocalDateTime dateEntree;
     private LocalDateTime dateSortie;
 
-    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Paiement> paiements;
 
+    public Ticket() {
+        this.dateEntree = LocalDateTime.now();
+        this.dateSortie = null;
+    }
+
     // Getters et setters
+    public Long getId() { return id; }
+
+    public LocalDateTime getDateEntree() { return dateEntree; }
+    public void setDateEntree(LocalDateTime dateEntree) { this.dateEntree = dateEntree; }
+
+    public LocalDateTime getDateSortie() { return dateSortie; }
+    public void setDateSortie(LocalDateTime dateSortie) { this.dateSortie = dateSortie; }
+
+    public List<Paiement> getPaiements() { return paiements; }
+    public void setPaiements(List<Paiement> paiements) { this.paiements = paiements; }
+
+    public void addPaiment(Paiement paiement) {
+        this.paiements.add(paiement);
+        paiement.setTicket(this);
+    }
 }
